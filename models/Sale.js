4f534a387648e5ef751ccb3d1d0c8ceae4a3cb1e
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-04-07 17:35:09 +0530

    Sale  model

---
+++ models/Sale.js
@@ -0,0 +1,34 @@
+
+const mongoose = require('mongoose');
+
+const SaleSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true
+  },
+  productName: {
+    type: String,
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  totalAmount: {
+    type: Number,
+    required: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Sale', SaleSchema);
